1f289a319403aba00b1787b18b0de8d3fe7c2cc9
swautele <swautle@student.s19.com>
2022-07-26 14:53:19 +0200

    save

---
+++ ex00/Template.hpp
@@ -0,0 +1,31 @@
+#ifndef TEMPLATE_HPP
+# define TEMPLATE_HPP
+
+# include <iostream>
+# include <string>
+
+template <class lol>
+void swap (lol a, lol b)
+{
+	lol tmp = b;
+	b = a;
+	a = tmp;
+};
+template <class lol>
+lol min (lol a, lol b)
+{
+	if (a < b)
+		return a;
+	else
+		return b;
+};
+template <class lol>
+lol max (lol a, lol b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+};
+
+#endif
